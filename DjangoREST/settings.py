63cc0de92e15381a47024c1c748d1b5c10e80f34
Mohammad Shaheduzzaman <58803282+mohammadshahed@users.noreply.github.com>
2021-06-26 16:17:35 +0600

    ok

--- DjangoREST/settings.py
+++ DjangoREST/settings.py
@@ -52,9 +52,6 @@ from datetime import timedelta
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
-    'BLACKLIST_AFTER_ROTATION': True,
-    'UPDATE_LAST_LOGIN': False,
 }
 
 MIDDLEWARE = [
